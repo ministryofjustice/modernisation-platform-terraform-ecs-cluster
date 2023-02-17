d59edbae77d59db6f9450a3329f9996bbb2a6531
George Taylor <george.taylor@digital.justice.gov.uk>
2023-02-17 15:53:03 +0000

    add ecs capacity provider

--- ecs-ec2/cluster.tf
+++ ecs-ec2/cluster.tf
@@ -24,3 +24,27 @@ resource "aws_ecs_capacity_provider" "this" {
     }
   )
 }
+
+resource "aws_ecs_capacity_provider" "this" {
+  name = "${var.name}-capacity-provider"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn         = aws_autoscaling_group.this.arn
+    managed_termination_protection = "ENABLED"
+
+    managed_scaling {
+      maximum_scaling_step_size = 1000
+      minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity           = 10
+    }
+  }
+
+  tags = merge(
+    var.tags_common,
+    {
+      Name = "${var.name}-capacity-provider"
+    }
+  )
+}
+
