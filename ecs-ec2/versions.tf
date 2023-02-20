d0d070a385312841650b298330abef64e71e0e0b
George Taylor <george.taylor@digital.justice.gov.uk>
2023-02-20 18:10:07 +0000

    Revert "remove unused k8s provider"
    This reverts commit 8768328632d3d99452688a3294f7e3b0d28c6e8f.

--- ecs-ec2/versions.tf
+++ ecs-ec2/versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">=4.0.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">=2.0.0"
+    }
   }
   required_version = ">= 0.14"
 }
