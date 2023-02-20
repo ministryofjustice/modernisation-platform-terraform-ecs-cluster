fff5f76e05b3b26f0eaf530d442708211e389994
George Taylor <george.taylor@digital.justice.gov.uk>
2023-02-20 18:11:23 +0000

    remove unused k8s provider

--- ecs-ec2/versions.tf
+++ ecs-ec2/versions.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = ">=4.0.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">=2.0.0"
-    }
   }
   required_version = ">= 0.14"
 }
