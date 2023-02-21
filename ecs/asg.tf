7b0d91ea6c032d8bb166bcfe0796324f6dc2ed0d
George Taylor <george.taylor@digital.justice.gov.uk>
2023-02-21 17:10:59 +0000

    Update ecs/asg.tf
    Co-authored-by: Kyle Hodgetts <kylehodgetts@gmail.com>

--- ecs/asg.tf
+++ ecs/asg.tf
@@ -1,5 +1,5 @@
 resource "aws_autoscaling_group" "this" {
-  count                 = var.ec2_capacity_enabled == true ? 1 : 0
+  count                 = var.ec2_capacity_enabled ? 1 : 0
   min_size              = var.min_size
   max_size              = var.max_size
   desired_capacity      = var.desired_capacity
