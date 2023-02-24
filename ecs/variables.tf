c663786320e2121e55ac6b53bc1ad50d58a83ebb
Kyle Hodgetts <kyle.hodgetts@digital.justice.gov.uk>
2023-02-24 13:42:43 +0000

    test cloudposse module

--- ecs/variables.tf
+++ ecs/variables.tf
@@ -1,125 +1,35 @@
-variable "instance_type" {
-  description = "ECS Instance Type"
-  type        = string
-  default     = "t3.large"
+variable "enable_ec2_capacity_provider" {
+  type = bool
+  default = false
+  description = "Enable EC2 capacity provider"
 }
 
-variable "subnet_ids" {
-  description = "Subnet IDs to use for the ECS cluster"
+variable "ec2_capacity_instance_type" {
+  default = "t3.medium"
+  type = string
+  description = "EC2 instance type for capacity provider"
+}
+
+variable "ec2_capacity_security_group_id" {
+  type = string
+  description = "EC2 capacity provider security group ID"
+  default = ""
+}
+
+variable "ec2_subnet_ids" {
+  description = "Subnet IDs for EC2 capacity provider"
   type        = list(string)
+  default = []
 }
 
-variable "vpc_id" {
-  description = "Value of the VPC ID"
-  type        = string
+variable "ec2_capacity_min_size" {
+  type = number
+  default = 1
+  description = "EC2 capacity provider min size"
 }
 
-variable "user_data" {
-  description = "User data to pass to the launch template"
-  type        = string
-}
-
-variable "ingress_rules" {
-  description = "Security group ingress rules for the cluster EC2s"
-  type = map(object({
-    description     = string
-    from_port       = number
-    to_port         = number
-    protocol        = string
-    security_groups = list(string)
-    cidr_blocks     = list(string)
-  }))
-}
-
-variable "egress_rules" {
-  description = "Security group egress rules for the cluster EC2s"
-  type = map(object({
-    description     = string
-    from_port       = number
-    to_port         = number
-    protocol        = string
-    security_groups = list(string)
-    cidr_blocks     = list(string)
-  }))
-}
-
-variable "log_retention_in_days" {
-  description = "Log retention in days"
-  type        = number
-  default     = 30
-}
-
-variable "name" {
-  description = "Cluster name"
-  type        = string
-}
-
-
-variable "tags_common" {
-  type        = map(string)
-  description = "Common tags to be used by all resources"
-  default = {
-    "key" = "value"
-  }
-}
-
-variable "key_name" {
-  description = "Key name to use for the cluster EC2s"
-  type        = string
-  default     = ""
-}
-
-variable "desired_capacity" {
-  description = "Desired capacity of the cluster"
-  type        = number
-  default     = 1
-}
-
-variable "min_size" {
-  description = "Minimum size of the cluster"
-  type        = number
-  default     = 1
-}
-
-variable "max_size" {
-  description = "Maximum size of the cluster"
-  type        = number
-  default     = 1
-}
-
-variable "ec2_capacity_enabled" {
-  description = "Enable EC2 capacity"
-  type        = bool
-  default     = false
-}
-
-variable "capacity_provider_maximum_scaling_step_size" {
-  description = "Maximum scaling step size for the capacity provider"
-  type        = number
-  default     = 1000
-}
-
-variable "capacity_provider_minimum_scaling_step_size" {
-  description = "Minimum scaling step size for the capacity provider"
-  type        = number
-  default     = 1
-}
-
-variable "capacity_provider_desired_capacity" {
-  description = "Desired capacity for the capacity provider"
-  type        = number
-  default     = 10
-}
-
-variable "enable_managed_scaling" {
-  description = "Enable managed scaling for the capacity provider"
-  type        = bool
-  default     = false
-}
-
-
-variable "enable_container_insights" {
-  description = "Enable container insights"
-  type        = bool
-  default     = false
+variable "ec2_capacity_max_size" {
+    type = number
+    default = 1
+    description = "EC2 capacity provider max size"
 }
--- test.tf
+++ test.tf
@@ -1,40 +1,73 @@
+#module "ecs" {
+#  source        = "./ecs"
+#  name          = "test"
+#  vpc_id        = "vpc-01d7a2da8f9f1dfec"
+#  subnet_ids    = ["subnet-04af8bd9dbbce3310", "subnet-0131824ef5a4ece01", "subnet-01815760b71d6a619"]
+#  instance_type = "t3.large"
+#  user_data = base64encode(
+#    <<-EOF
+#    #!/bin/bash -x
+#    exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+#      echo ECS_CLUSTER=test >> /etc/ecs/ecs.config
+#    EOF
+#  )
+#  ingress_rules = {
+#    "all" = {
+#      description     = "All"
+#      from_port       = 0
+#      to_port         = 0
+#      protocol        = "-1"
+#      security_groups = []
+#      cidr_blocks = [
+#        "0.0.0.0/0"
+#      ]
+#    }
+#  }
+#  egress_rules = {
+#    "all" = {
+#      description     = "All"
+#      from_port       = 0
+#      to_port         = 0
+#      protocol        = "-1"
+#      security_groups = []
+#      cidr_blocks = [
+#        "0.0.0.0/0"
+#      ]
+#    }
+#  }
+#}
+
+data "aws_subnet" "private_subnets_a" {
+  vpc_id = "vpc-01d7a2da8f9f1dfec"
+  tags = {
+    "Name" = "hmpps-development-general-private-eu-west-2a"
+  }
+}
+
+data "aws_subnet" "private_subnets_b" {
+  vpc_id = "vpc-01d7a2da8f9f1dfec"
+  tags = {
+    "Name" = "hmpps-development-general-private-eu-west-2b"
+  }
+}
+
+data "aws_subnet" "private_subnets_c" {
+  vpc_id = "vpc-01d7a2da8f9f1dfec"
+  tags = {
+    "Name" = "hmpps-development-general-private-eu-west-2c"
+  }
+}
+
 module "ecs" {
-  source        = "./ecs"
-  name          = "test"
-  vpc_id        = "vpc-01d7a2da8f9f1dfec"
-  subnet_ids    = ["subnet-04af8bd9dbbce3310", "subnet-0131824ef5a4ece01", "subnet-01815760b71d6a619"]
-  instance_type = "t3.large"
-  user_data = base64encode(
-    <<-EOF
-    #!/bin/bash -x
-    exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-      echo ECS_CLUSTER=test >> /etc/ecs/ecs.config
-    EOF
-  )
-  ingress_rules = {
-    "all" = {
-      description     = "All"
-      from_port       = 0
-      to_port         = 0
-      protocol        = "-1"
-      security_groups = []
-      cidr_blocks = [
-        "0.0.0.0/0"
-      ]
-    }
-  }
-  egress_rules = {
-    "all" = {
-      description     = "All"
-      from_port       = 0
-      to_port         = 0
-      protocol        = "-1"
-      security_groups = []
-      cidr_blocks = [
-        "0.0.0.0/0"
-      ]
-    }
-  }
+  source = "./ecs"
+  name = "test-jitbit"
+  enable_ec2_capacity_provider = true
+  ec2_capacity_security_group_id = "sg-039641b55accdef23"
+  ec2_subnet_ids = [
+    data.aws_subnet.private_subnets_a.id,
+    data.aws_subnet.private_subnets_b.id,
+    data.aws_subnet.private_subnets_c.id
+  ]
 }
 
 provider "aws" {
