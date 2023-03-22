fe436a8e9e5af0abfce8c84e409c3547439efcf4
George Taylor <george.taylor@digital.justice.gov.uk>
2023-03-22 15:51:10 +0000

    context fix

--- cluster/main.tf
+++ cluster/main.tf
@@ -18,5 +18,6 @@ module "ecs_cluster" {
   capacity_providers_fargate      = true
   capacity_providers_fargate_spot = true
   capacity_providers_ec2          = local.ec2_capacity_provider
+  context                         = module.this.context
 }
 
