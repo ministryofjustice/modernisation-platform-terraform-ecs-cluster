f6fb13c4e5c24ab193987459537658bfcd77e4e8
George Taylor <george.taylor@digital.justice.gov.uk>
2023-04-14 11:45:38 +0100

    refactor

--- cluster/main.tf
+++ cluster/main.tf
@@ -1,18 +1,3 @@
-locals {
-  ec2_capacity_provider = var.enable_ec2_capacity_provider ? {
-    ec2_default = {
-      instance_type                  = var.ec2_capacity_instance_type
-      security_group_ids             = [var.ec2_capacity_security_group_id]
-      subnet_ids                     = var.ec2_subnet_ids
-      associate_public_ip_address    = false
-      min_size                       = var.ec2_capacity_min_size
-      max_size                       = var.ec2_capacity_max_size
-      metadata_http_tokens_required  = var.metadata_http_tokens_required
-      metadata_http_endpoint_enabled = var.metadata_http_endpoint_enabled
-    }
-  } : {}
-}
-
 module "ecs_cluster" {
   #checkov:skip=CKV_AWS_79: http endpoint and http tokens configured to enable IMDSv2 by default. 
   source = "../cloudposse/ecs-cluster/aws"
