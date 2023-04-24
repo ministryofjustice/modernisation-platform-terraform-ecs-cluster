ccb2f87b1bdafd32a9b4053315463ff7960c227e
George Taylor <george.taylor@digital.justice.gov.uk>
2023-04-24 11:09:16 +0100

    typo

--- cluster/main.tf
+++ cluster/main.tf
@@ -1,5 +1,5 @@
 module "ecs_cluster" {
-  #checkov:skip=CKV_AWS_79: http endpoint and http tokens configured to enable IMDSv2 by default in `local.ec2_capacity_providers`
+  #checkov:skip=CKV_AWS_79: http endpoint and http tokens configured to enable IMDSv2 by default in `local.ec2_capacity_provider`
   source = "../cloudposse/ecs-cluster/aws"
 
   container_insights_enabled      = true
