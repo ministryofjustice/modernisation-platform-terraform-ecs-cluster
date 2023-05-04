b4849849c06d61689ee0163489d64d61193285f5
George Taylor <george.taylor@digital.justice.gov.uk>
2023-05-04 13:42:43 +0100

    test uuid

--- service/main.tf
+++ service/main.tf
@@ -43,7 +43,7 @@ module "ecs_alb_service_task" {
 
   ecs_load_balancers         = var.ecs_load_balancers
   use_alb_security_group     = true
-  force_new_deployment       = false
+  force_new_deployment       = true
   redeploy_on_apply          = true
   deployment_controller_type = "ECS"
   propagate_tags             = "SERVICE"
