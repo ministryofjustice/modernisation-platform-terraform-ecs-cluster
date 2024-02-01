62e46cb75e6b6e2e18abf04685979cf392b6f566
George Taylor <george.taylor@digital.justice.gov.uk>
2024-02-01 16:03:51 +0000

    output private_dns_namespace arn

--- cluster/outputs.tf
+++ cluster/outputs.tf
@@ -1,3 +1,15 @@
 output "ecs_cluster_arn" {
   value = module.ecs_cluster.arn
 }
+
+output "ecs_cluster_name" {
+  value = module.ecs_cluster.name
+}
+
+output "ecs_cluster_id" {
+  value = module.ecs_cluster.id
+}
+
+output "cluster_private_dns_namespace_arn" {
+  value = aws_service_discovery_private_dns_namespace.this[0].arn
+}
