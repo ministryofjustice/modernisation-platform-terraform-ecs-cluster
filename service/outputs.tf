710e3d9978564b75aeeee2ee3ad4218a5573267d
George Taylor <george.taylor@digital.justice.gov.uk>
2024-03-13 17:12:35 +0000

    Update outputs.tf

--- service/outputs.tf
+++ service/outputs.tf
@@ -1,4 +1,4 @@
 output "service_arn" {
-  value       = var.ignore_changes ? aws_ecs_service.ignore_changes[0].arn : aws_ecs_service.default[0].arn
+  value       = var.ignore_changes ? aws_ecs_service.ignore_changes[0].id : aws_ecs_service.default[0].id
   description = "The ARN for the ECS Service"
 }
