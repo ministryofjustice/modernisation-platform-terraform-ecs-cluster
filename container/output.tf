45dd510c4f6a8e500f3e7c2ef7b7e9c30450d76a
George Taylor <george.taylor@digital.justice.gov.uk>
2024-03-13 22:13:45 +0000

    Update output.tf

--- container/output.tf
+++ container/output.tf
@@ -1,4 +1,10 @@
 output "json_encoded" {
-  value     = local.container_definition
+  value     = local.container_definition_json
   sensitive = false
 }
+
+output "json_encoded_list" {
+  value     = local.container_definition_list
+  sensitive = false
+}
+
