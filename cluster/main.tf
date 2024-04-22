31c7c83d401c40a9a9c1941806bd2be940580c8f
George Taylor <george.taylor@digital.justice.gov.uk>
2024-04-22 14:44:24 +0100

    Update main.tf (#125)

--- cluster/main.tf
+++ cluster/main.tf
@@ -5,4 +5,10 @@ resource "aws_ecs_cluster" "this" {
     name  = "containerInsights"
     value = var.enable_container_insights
   }
+
+  configuration {
+    execute_command_configuration {
+      logging = "DEFAULT"
+    }
+  }
 }
